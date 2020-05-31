cbea848db408c15db4a6f1bb070119d1422a58b3
Dariusz <57624006+dlwas@users.noreply.github.com>
2020-05-31 18:47:21 +0200

    Update SectionHeader.vue

--- src/components/SectionHeader.vue
+++ src/components/SectionHeader.vue
@@ -73,12 +73,12 @@
             z-index: -99;
             width: 100%;
             height: 80%;
-            opacity: 8%;
             background-image: url('../assets/header-background.webp');
             background-repeat: repeat-x;
             background-position: top;
             background-size: auto;
             background-attachment: fixed;
+            opacity: 10%;
         }
     }
 
