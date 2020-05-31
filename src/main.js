0ed5af9ec9cf5b9f65840feab77bbe97ee060bdd
Dariusz <57624006+dlwas@users.noreply.github.com>
2020-05-31 18:24:17 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import i18n from './i18n'
 // VueScrollTo
 import VueScrollTo from 'vue-scrollto'
 Vue.use(VueScrollTo)
+// db
+import projects from '@/assets/db/dataProjects.json'
+import root from '@/assets/db/dataRoot.json'
+Vue.prototype.$db = {
+  root,
+  projects
+}
 
 Vue.config.productionTip = false
 
