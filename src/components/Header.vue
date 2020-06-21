3475115e61e7093309f33e06edbb11f9fa4ec946
Dariusz <57624006+dlwas@users.noreply.github.com>
2020-06-21 21:34:45 +0200

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -77,7 +77,7 @@
             background-position: top;
             background-size: auto;
             background-attachment: fixed;
-            opacity: 0.05;
+            opacity: 0.08;
         }
     }
 
